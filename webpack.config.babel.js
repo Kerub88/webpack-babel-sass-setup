696945426cbdb35b6ffa6d7ea75bb907b6880abf
unknown <kacsa1984@gmail.com>
2019-06-24 17:57:13 +0200

    setup: webpack, babel, sass configured

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,38 +1,47 @@
-import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import ScriptExtHtmlWebpackPlugin from 'script-ext-html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+
 export default {
-    entry: path.join(__dirname, 'src/index.js'),
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js/,
-            exclude: /(node_modules|bower_components)/,
-            use: [{
-                loader: 'babel-loader'
-            }]
-        }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Custom template',
-            template: path.join(__dirname, 'src/index.template.html')
-        }),
-        new ScriptExtHtmlWebpackPlugin({
-            defaultAttribute: 'defer'
-        })
-    ],
-    stats: {
-        colors: true
-    },
-    devtool: 'source-map', 
-    mode:"development",
-    devServer: {
-            contentBase: './dist',
-            inline:true,
-            port: 3000 //my prefered port for development, but change as you see fit
-    }
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [{
+					loader: 'html-loader',
+					options: { minimize: true }
+				}]
+			},
+			{
+        test: /\.s(a|c)ss$/,
+        use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader"
+				]
+      }
+		]
+	},
+
+	resolve: {
+    extensions: ['.js', '.scss']
+	},
+	
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Custom template',
+			template: "./src/index.html",
+			filename: "./index.html"
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+			chunkFilename: '[id].css'
+		})
+	]
 };
